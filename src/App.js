07a707f1aefe982a9e682dd7500eda0a070ac620
Death <mathrox431@yahoo.com>
2019-06-01 23:18:21 -0400

    Added bootstrap 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header className="masthead mb-auto">
+          <div className="inner">
+            <h3 className="masthead-brand">Cover</h3>
+            <nav className="nav nav-masthead justify-content-center">
+              <a className="nav-link active" href="#">
+                Home
+              </a>
+              <a className="nav-link" href="#">
+                Features
+              </a>
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </nav>
+          </div>
+        </header>
+
+        <main role="main" className="inner cover">
+          <h1 className="cover-heading">React with Bootstrap</h1>
+          <p className="lead">
+            Cover is a one-page template for building simple and beautiful home
+            pages. Download, edit the text, and add your own fullscreen
+            background photo to make it your own.
+          </p>
+          <p className="lead">
+            <a href="#" className="btn btn-lg btn-secondary">
+              Learn more
+            </a>
+          </p>
+        </main>
+
+        <footer className="mastfoot mt-auto">
+          <div className="inner">
+            <p>
+              Cover template for{" "}
+              <a href="https://getbootstrap.com/">Bootstrap</a>, by{" "}
+              <a href="https://twitter.com/mdo">@mdo</a>.
+            </p>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
